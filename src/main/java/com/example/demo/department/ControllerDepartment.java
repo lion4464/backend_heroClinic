7d81196382c2b433f3e345717a646e0beaf2f303
Rustambek <herofirst4464@gmail.com>
2022-07-29 01:04:51 +0500

    Bugfix(#4) Swagger added

--- src/main/java/com/example/demo/department/ControllerDepartment.java
+++ src/main/java/com/example/demo/department/ControllerDepartment.java
@@ -1,6 +1,9 @@
 package com.example.demo.department;
 
+import com.example.demo.configuration.SwaggerUI;
 import com.example.demo.configuration.UserDetailsImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -25,24 +28,29 @@ public class ControllerDepartment {
 
 
     @PostMapping("/save")
+    @Operation(security = {@SecurityRequirement(name = SwaggerUI.AccessToken)},summary = "")
     public ResponseEntity<DepartmentDto> save(@RequestBody DepartmentRequest request){
         UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
        return new ResponseEntity<>(departmentmapper.toDto(departmentService.saveRole(userDetails.getUserEntity(),request)), HttpStatus.OK);
     }
     @GetMapping("/all")
+    @Operation(security = {@SecurityRequirement(name = SwaggerUI.AccessToken)})
     public ResponseEntity<List<DepartmentDto>> all(){
         UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return ResponseEntity.ok().body(departmentmapper.fromPageEntity(departmentService.all(userDetails.getUserEntity())));
     }
     @PutMapping("/update")
+    @Operation(security = {@SecurityRequirement(name = SwaggerUI.AccessToken)})
     public ResponseEntity<DepartmentDto> update(@Valid @RequestBody DepartmentRequest obj){
         return ResponseEntity.ok().body(departmentmapper.toDto(departmentService.update(obj)));
     }
     @GetMapping("get/{id}")
+    @Operation(security = {@SecurityRequirement(name = SwaggerUI.AccessToken)})
     public ResponseEntity<DepartmentDto> get(@PathVariable("id") UUID id){
         return ResponseEntity.ok().body(departmentmapper.toDto(departmentService.get(id)));
     }
     @DeleteMapping("/delete/{id}")
+    @Operation(security = {@SecurityRequirement(name = SwaggerUI.AccessToken)})
     public ResponseEntity<String> delete(@PathVariable("id") UUID id){
         return ResponseEntity.ok(departmentService.delete(id));
     }
